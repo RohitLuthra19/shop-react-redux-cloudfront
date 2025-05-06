425acea459e596ad2908c822e431ec050f7fd572
Rohit Kumar <rohit.kumar@clario.com>
2025-05-06 16:08:10 +0530

    integrated deployed apis

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,6 +1,6 @@
 const API_PATHS = {
   product:
-    "https://z4jqf7upf7.execute-api.us-east-1.amazonaws.com/prod/products",
+    "https://cp7oo9eim9.execute-api.us-east-1.amazonaws.com/prod/products",
   order: "https://.execute-api.eu-west-1.amazonaws.com/dev",
   import: "https://.execute-api.eu-west-1.amazonaws.com/dev",
   bff: "https://.execute-api.eu-west-1.amazonaws.com/dev",
